3faaec1f4348a9d331c3ace8c7ad9c0c1146acdd
DS03-hash <seredin11.03.2002@gmail.com>
2026-02-19 20:16:59 +0300

    first version with post and get handlers

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var task string
+
+type requestBody struct {
+	Task string `json:"task"`
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	var body requestBody
+	json.NewDecoder(r.Body).Decode(&body)
+
+	task = body.Task
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("task saved"))
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("hello, %s", task)))
+}
+
+func main() {
+	http.HandleFunc("/task", postHandler)
+	http.HandleFunc("/", getHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
