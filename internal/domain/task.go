9217c2c864ee447e0829e672aaba1b2b3d585d35
DS03-hash <seredin11.03.2002@gmail.com>
2026-03-06 01:20:39 +0300

    add comment to all code

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -8,3 +8,5 @@ type Task struct {
 	IsDone    bool           `json:"is_done"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
+// Сущность обработки
+// Task - это структура, которая представляет собой задачу. Она содержит поля ID, Task, IsDone и DeletedAt. ID - это уникальный идентификатор задачи, Task - это текст задачи, IsDone - это булевое значение, которое указывает, выполнена ли задача, а DeletedAt - это поле для мягкого удаления задачи.
